132c2636d82aae5eb823714a3a7948cc6d5be66c
ItsLolgical <signupthing@itslolgical.com>
2023-11-04 21:23:46 -0500

    Initial commit.

---
+++ CryptographicPasswordStorage/Code/CryptographyHandler.cs
@@ -0,0 +1,114 @@
+﻿using System.Security.Authentication;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographicPasswordStorage.Code;
+
+public static class CryptographyHandler
+{
+    private const int KeySize = 64;
+    private const int Iterations = 350000;
+    private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA512;
+
+    public static string HashPassword(string argPassword, out byte[] salt)
+    {
+        salt = RandomNumberGenerator.GetBytes(KeySize);
+
+        var hash = Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(argPassword),
+            salt,
+            Iterations,
+            Algorithm,
+            KeySize);
+        return Convert.ToHexString(hash);
+    }
+
+    public static bool VerifyPassword(string argPassword, string argHash, byte[] argSalt)
+    {
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(argPassword, argSalt, Iterations, Algorithm, KeySize);
+        return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromHexString(argHash));
+    }
+
+    public static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+    {
+        // Check arguments.
+        if (plainText == null || plainText.Length <= 0)
+            throw new ArgumentNullException("plainText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+        byte[] encrypted;
+
+        // Create an Aes object
+        // with the specified key and IV.
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for encryption.
+            using (var msEncrypt = new MemoryStream())
+            {
+                using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (var swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+        }
+
+        // Return the encrypted bytes from the memory stream.
+        return encrypted;
+    }
+
+    public static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+    {
+        // Check arguments.
+        if (cipherText == null || cipherText.Length <= 0)
+            throw new ArgumentNullException("cipherText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+
+        // Declare the string used to hold
+        // the decrypted text.
+        string plaintext = null;
+
+        // Create an Aes object
+        // with the specified key and IV.
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        return plaintext;
+    }
+}
---
+++ CryptographicPasswordStorage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+using Spectre.Console;
+using CryptographicPasswordStorage.Code;
+
+AnsiConsole.Write(new FigletText("Cryptography .NET Demo").Centered().Color(Color.Green));
+
+var password = "itsAPassword";
+
+byte[] salt;
+
+var hashedPassword = CryptographyHandler.HashPassword(password, out salt);
+
+var isVerifiable = CryptographyHandler.VerifyPassword(password, hashedPassword, salt);
+
+Console.WriteLine($"Original Password: {password}");
+Console.WriteLine($"Hashed Password:   {hashedPassword}");
+Console.WriteLine($"Salt:              {salt.Length}");
+Console.WriteLine($"Verified:          {isVerifiable}");
+
+Console.WriteLine();
+
+var original = "Here is some data to encrypt! Can it be decrypted?";
+
+// Create a new instance of the Aes
+// class.  This generates a new key and initialization
+// vector (IV).
+using (var myAes = Aes.Create())
+{
+    // Encrypt the string to an array of bytes.
+    var encrypted = CryptographyHandler.Encrypt(original, myAes.Key, myAes.IV);
+
+    // Decrypt the bytes to a string.
+    var roundtrip = CryptographyHandler.Decrypt(encrypted, myAes.Key, myAes.IV);
+
+    //Display the original data and the decrypted data.
+    Console.WriteLine("Original:   {0}", original);
+    Console.WriteLine("Encrypted:  {0}", encrypted.Length);
+    Console.WriteLine("Round Trip: {0}", roundtrip);
+}
